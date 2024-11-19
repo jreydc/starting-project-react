70078850707dfc24b2910c3c2301db1936f0a97c
jreydc <jreydc@gmail.com>
2024-11-19 17:10:00 +0800

    Updated the Core Concepts

---
+++ src/assets/AssetLoader.jsx
@@ -0,0 +1,12 @@
+import reactIMG from "./react-core-concepts.png";
+import componentIMG from "./components.png";
+import configIMG from "./config.png";
+import jsxIMG from "./jsx-ui.png";
+import stateIMG from "./state-mgmt.png";
+
+// Export the imported images
+export const reactLoadingIMG = reactIMG;
+export const componentLoadingIMG = componentIMG;
+export const configLoadingIMG = configIMG;
+export const jsxLoadingIMG = jsxIMG;
+export const stateLoadingIMG = stateIMG;
